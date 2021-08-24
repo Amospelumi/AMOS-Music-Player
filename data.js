d24f8255ee4423b728955a9ae73d40dd6abcda57
Amospelumi <oluwapelumiabiodun45@gmail.com>
2021-08-24 13:18:19 +0100

    New App

---
+++ data.js
@@ -0,0 +1,31 @@
+// All song in this app stored in an array.
+
+let songs = [
+    {
+        name: 'Lie',
+        path: 'Assets/musics/Kizz-Daniel-Lie.mp3',
+        artist: 'kizz daniel',
+        cover: 'Assets/images/Kizz-Daniel-Lie.jpg'
+    },
+
+    {
+        name: 'Peru',
+        path: 'Assets/musics/Buju-Fireboy-Peru-Refix.mp3',
+        artist: 'Buju ft Fireboy',
+        cover: 'Assets/images/buju-fireboy.jpg'
+    },
+
+    {
+        name: 'Let Me Know',
+        path: 'Assets/musics/Mayorkun-Let-Me-Know.mp3',
+        artist: 'Mayorkun',
+        cover: 'Assets/images/Mayorkun-Let-Me-Know.jpeg'
+    },
+
+    {
+        name: 'Lie',
+        path: 'Assets/musics/Kizz-Daniel-Lie.mp3',
+        artist: 'Oxlade',
+        cover: 'Assets/images/oxlade.jpeg'
+    }
+]
